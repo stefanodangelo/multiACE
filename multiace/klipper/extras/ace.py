490eda2feef88cea0309489ac0e743f3d9ccc718
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2026-09-03 18:10:46 +0200

    chore: update MULTIACE_BUILD_TAG to ec9dc95

--- multiace/klipper/extras/ace.py
+++ multiace/klipper/extras/ace.py
@@ -22,7 +22,7 @@ MULTIACE_CODENAME = "Clean Sweep"
 
 ACE_API_VERSION = 1
 
-MULTIACE_BUILD_TAG = "5af9d74"
+MULTIACE_BUILD_TAG = "ec9dc95"
 MULTIACE_BUNDLE_SHA1 = "9ba7849"
 
 def _load_i18n_catalog(i18n_dir, lang):
